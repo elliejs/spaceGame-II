067323353208f05375b25927f19e09063d7f80d4
elliejs <ejsilver@wpi.edu>
2022-03-10 22:42:12 -0500

    grouping parenthesis

--- nblessings/nblessings.h
+++ nblessings/nblessings.h
@@ -16,7 +16,7 @@ pixel_t;
 #define APPLY_COLOR_SIZE 11
 #define PIXEL_CHAR_SIZE 3
 
-#define MAX_FRAMEBUFFER_SIZE(W, H) CLEAR_SCREEN_SIZE + RESET_HOME_SIZE + (unsigned int) (W) * (unsigned int) (H) * (2 * APPLY_COLOR_SIZE + PIXEL_CHAR_SIZE) + 1
+#define MAX_FRAMEBUFFER_SIZE(W, H) (CLEAR_SCREEN_SIZE + RESET_HOME_SIZE + (unsigned int) (W) * (unsigned int) (H) * (2 * APPLY_COLOR_SIZE + PIXEL_CHAR_SIZE) + 1)
 
 unsigned char * nblessings_header_data(oklab_t * colors, unsigned int num_colors, unsigned int * header_len);
 unsigned char * nblessings_footer_data(unsigned int * footer_len);
