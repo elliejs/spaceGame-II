cf3574432602ebaa11a83231b90eb49c7ecfa115
elliejs <ejsilver@wpi.edu>
2023-02-22 21:54:14 -0500

    stubbed out the user_database until aa_tree is fixed
    aa_tree needs to be position independent or remappings of it won't work.

--- users/user_db.c
+++ users/user_db.c
@@ -4,7 +4,7 @@
 #include <fcntl.h>
 #include <sys/mman.h>
 #include <unistd.h>
-
+#include <errno.h>
 
 #include "user_db.h"
 #include "../utils/semaphore.h"
@@ -60,6 +60,7 @@ compare_t user_comparator(void * a, void * b) {
 }
 
 void start_user_db(void) {
+  return;
     if (user_db != NULL) {
     printf("[user_db]: Already instantiated, not double-mallocing\n");
     return;
@@ -68,14 +69,56 @@ void start_user_db(void) {
   user_db = (user_db_t *) mmap(NULL, sizeof(user_db_t), PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0);
   RWLOCK_INIT(&(user_db->rwlock_data));
   RWLOCK_INIT(&(user_db->rwlock_index));
+  bool index_exists = false;
+  bool data_exists = false;
+  user_db->user_index_fd = open("users/user_index.database", O_RDWR | O_CREAT | O_EXCL, S_IRUSR | S_IWUSR);
+  if ((user_db->user_index_fd == -1) && (errno == EEXIST)) {
+    user_db->user_index_fd = open("users/user_index.database", O_RDWR | O_CREAT, S_IRUSR | S_IWUSR);
+    index_exists = true;
+  }
 
-  user_db->user_index_fd = open("users/user_index.database", O_RDWR | O_CREAT, S_IRUSR | S_IWUSR);
-  user_db->user_data_fd = open("users/user_data.database", O_RDWR | O_CREAT, S_IRUSR | S_IWUSR);
-
-  read(user_db->user_index_fd, &(user_db->num_users),  sizeof(unsigned int));
-  user_db->max_users = fminl(user_db->num_users * 2, MIN_DB_SIZE);
+  user_db->user_data_fd = open("users/user_data.database", O_RDWR | O_CREAT | O_EXCL, S_IRUSR | S_IWUSR);
+  if ((user_db->user_data_fd == -1) && (errno == EEXIST)) {
+    user_db->user_data_fd = open("users/user_data.database", O_RDWR | O_CREAT, S_IRUSR | S_IWUSR);
+    data_exists = true;
+  }
+  printf("should have opened both database files by now\n");
+  if (index_exists ^ data_exists) {
+    printf("[user_db]: users database corrupted.\n\tuser_index.database file %s exist.\n\tuser_data.database file %s exist.\n\tServer Corrupted. Delete all database files or restore from backup.",
+           index_exists ? "DOES" : "DOES NOT",
+           data_exists ? "DOES" : "DOES NOT");
+  }
+  if (!index_exists && !data_exists) {
+    printf("num_users 0 since new db\n");
+    user_db->num_users = 0;
+  } else {
+    read(user_db->user_index_fd, &(user_db->num_users),  sizeof(unsigned int));
+  }
+  user_db->max_users = fmaxl(user_db->num_users * 2, MIN_DB_SIZE);
+  printf("max users %d\n", user_db->max_users);
   user_db->user_index = mmap(NULL, user_db->max_users * sizeof(user_t) + sizeof(user_index_t), PROT_READ | PROT_WRITE, MAP_SHARED, user_db->user_index_fd, 0);
   user_db->user_data = mmap(NULL, user_db->max_users * sizeof(user_data_t), PROT_READ | PROT_WRITE, MAP_SHARED, user_db->user_data_fd, 0);
+
+  if (!index_exists && !data_exists) {
+    printf("making new user_index aa_tree\n");
+    user_db->user_index->num_users = 0;
+    user_db->user_index->data = (aa_tree_t) {
+      .comparator = user_comparator,
+      .nil = (aa_node_t) {
+        .left = &(user_db->user_index->data.nil),
+        .right = &(user_db->user_index->data.nil),
+        .data = NULL,
+        .level = 0
+      },
+      .root = &(user_db->user_index->data.nil)
+    };
+
+    printf("all done!\n");
+    const long PAGESIZE = sysconf(_SC_PAGESIZE);
+    uintptr_t page_aligned_offset = (uintptr_t) user_db->user_index % PAGESIZE;
+    uintptr_t page_aligned_addr = (uintptr_t) user_db->user_index - page_aligned_offset;
+    msync((void *) page_aligned_addr, page_aligned_offset + sizeof(user_index_t), MS_ASYNC);
+  }
 }
 
 void end_user_db(void) {
@@ -89,9 +132,10 @@ void end_user_db(void) {
 }
 
 off_t get_user(char const * name) {
+  printf("[user_db]: get_user(%s)\n", name);
   aa_node_t * result = NULL;
   user_t user;
-  memcpy(user.username, name, MAX_USERPASS_LEN);
+  strncpy(user.username, name, MAX_USERPASS_LEN);
   RWLOCK_RLOCK(&(user_db->rwlock_index));
   if (aa_find(&(user_db->user_index->data), (void *) &user, &result)) {
     RWLOCK_RUNLOCK(&(user_db->rwlock_index));
@@ -106,13 +150,20 @@ void update_user_data(off_t user_offset, object_t * object) {
   const long PAGESIZE = sysconf(_SC_PAGESIZE);
   RWLOCK_WLOCK(&(user_db->rwlock_data));
     memcpy(&(user_db->user_data[user_offset].self), object, sizeof(object_t));
-    uintptr_t page_aligned_addr = (uintptr_t) user_db->user_data + user_offset / PAGESIZE;
-    uintptr_t page_aligned_offset = (uintptr_t) user_db->user_data + user_offset % PAGESIZE;
+    uintptr_t page_aligned_offset = (uintptr_t) (user_db->user_data + user_offset) % PAGESIZE;
+    uintptr_t page_aligned_addr = (uintptr_t) (user_db->user_data + user_offset) - page_aligned_offset;
     msync((void *) page_aligned_addr, page_aligned_offset + sizeof(user_data_t), MS_ASYNC);
   RWLOCK_WUNLOCK(&(user_db->rwlock_data));
 }
 
 void get_user_data(off_t user_offset, object_t * object) {
+  *object = create_ship((SGVec3D_t) {
+      .x = SGVec_Load_Const(CHUNK_SIZE / 2.),
+      .y = SGVec_Load_Const(CHUNK_SIZE / 2.),
+      .z = SGVec_Load_Const(CHUNK_SIZE / 2.)
+    },
+    (chunk_coord_t) {0,0,0});
+  return;
   RWLOCK_RLOCK(&(user_db->rwlock_data));
     memcpy(object, &(user_db->user_data[user_offset].self), sizeof(object_t));
   RWLOCK_RUNLOCK(&(user_db->rwlock_data));
@@ -139,17 +190,19 @@ off_t create_user(char const * username, char const * password) {
 
   RWLOCK_WLOCK(&(user_db->rwlock_index));
     user_t * free_user = user_db->user_index->backing_nodes + user_db->user_index->num_users;
-    memcpy(free_user->username, username, MAX_USERPASS_LEN);
+    strncpy(free_user->username, username, MAX_USERPASS_LEN);
     user_offset = free_user->offset = user_db->user_index->num_users;
     aa_insert(&(user_db->user_index->data), (void *) free_user, &(free_user->node));
     void * new_user_addr = (void *) free_user;
-    page_aligned_addr = (uintptr_t) new_user_addr / PAGESIZE;
-    page_aligned_offset = (uintptr_t) new_user_addr % PAGESIZE;
 
+    page_aligned_offset = (uintptr_t) new_user_addr % PAGESIZE;
+    page_aligned_addr = (uintptr_t) new_user_addr - page_aligned_offset;
     msync((void *) page_aligned_addr, page_aligned_offset + sizeof(user_t), MS_ASYNC);
+
     user_db->user_index->num_users++;
-    page_aligned_addr = (uintptr_t) &(user_db->user_index->num_users) / PAGESIZE;
+
     page_aligned_offset = (uintptr_t) &(user_db->user_index->num_users) % PAGESIZE;
+    page_aligned_addr = (uintptr_t) &(user_db->user_index->num_users) - page_aligned_offset;
     msync((void *) page_aligned_addr, page_aligned_offset + sizeof(user_db->user_index->num_users), MS_ASYNC);
   RWLOCK_WUNLOCK(&(user_db->rwlock_index));
 
@@ -171,7 +224,9 @@ off_t create_user(char const * username, char const * password) {
 
 
 off_t login(char const * username, char const * password) {
+  return 0;
   off_t user_offset = get_user(username);
+  printf("[user_db]: user_offset: %ld\n", user_offset);
   if (user_offset == -1) {
     return create_user(username, password);
   } else {
